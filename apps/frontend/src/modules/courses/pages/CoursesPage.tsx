cdd5963abb5d4b751ded50aabf31dead3e57b286
thebuuba <natanaelpena012@gmail.com>
2026-09-07 06:27:01 -0400

    Corrige import roto del visor de actividades

--- apps/frontend/src/modules/courses/pages/CoursesPage.tsx
+++ apps/frontend/src/modules/courses/pages/CoursesPage.tsx
@@ -3,7 +3,7 @@ import { useSearchParams } from 'react-router-dom'
 import './subject-activities-actions.css'
 import { CoursesPage as CoursesPageBase } from './CoursesPageBase'
 import { GroupedSubjectActivitiesPage } from './GroupedSubjectActivitiesPage'
-import { SubjectActivityDetailModal } from './SubjectActivityDetailModal'
+import { SubjectAttendancePage } from './SubjectAttendancePage'
 
 export {
   ActivityBlockPickerDialog,
@@ -17,15 +17,10 @@ export function CoursesPage() {
   const [searchParams] = useSearchParams()
   const courseId = searchParams.get('courseId') ?? ''
   const subjectId = searchParams.get('subjectId') ?? ''
-  const activityId = searchParams.get('activityId') ?? ''
   const hasSubjectContext = Boolean(courseId && subjectId)
   const tab = searchParams.get('tab')
 
-  if (hasSubjectContext && tab === 'actividades') {
-    return <>
-      <GroupedSubjectActivitiesPage />
-      {activityId ? <SubjectActivityDetailModal sectionSubjectId={subjectId} courseId={courseId} activityId={activityId} /> : null}
-    </>
-  }
+  if (hasSubjectContext && tab === 'actividades') return <GroupedSubjectActivitiesPage />
+  if (hasSubjectContext && tab === 'asistencia') return <SubjectAttendancePage />
   return <CoursesPageBase />
 }
