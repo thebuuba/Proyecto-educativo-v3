4cafb6269776c20ad85688ef7434e6d74ca0ceb6
thebuuba <natanaelpena012@gmail.com>
2026-09-15 13:47:06 -0400

    Activa estilos de cabecera interna de cursos

--- apps/frontend/src/modules/courses/pages/CoursesPage.tsx
+++ apps/frontend/src/modules/courses/pages/CoursesPage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
 import '../course-card-colors.css'
+import '../course-workspace-header.css'
 import './subject-activities-actions.css'
 import { CoursesPage as CoursesPageBase } from './CoursesPageBase'
 import { GroupedSubjectActivitiesPage } from './GroupedSubjectActivitiesPage'
